7e96ab32e0919fd73787b29071777e7389e60cfd
Tudor <tudor.gurau@mail.mcgill.ca>
2020-03-26 21:21:25 -0400

    added local txt file

--- app/src/main/java/com/example/judyy/grandnapoleonsolitairegame/VictoryScreen.java
+++ app/src/main/java/com/example/judyy/grandnapoleonsolitairegame/VictoryScreen.java
@@ -6,6 +6,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class VictoryScreen extends AppCompatActivity {
     @Override
@@ -14,7 +26,33 @@ public class VictoryScreen extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.game_stats);
-    }
 
+        StringBuilder info = new StringBuilder();
+        try {
+            File input = new File("./gamedata.txt");
+            BufferedReader reader = new BufferedReader(new FileReader(input));
+            String line;
+            while ((line = reader.readLine()) != null){
+                System.out.println("line is" + line);
+                info.append(line);
+                info.append("\n");
+            }
+            reader.close();
+        }
+        catch(FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println("text is: " + info);
+
+        TextView points = (TextView)findViewById(R.id.total_points);
+        points.setText(info);
+        TextView moves = (TextView)findViewById(R.id.total_moves);
+        moves.setText(info);
+
+
+    }
 
 }
